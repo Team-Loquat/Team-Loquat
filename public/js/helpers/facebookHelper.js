0d9ecd9d332cfe96771b4795079770f0273e3a18
2NSoft <nikolay.i.nachev@gmail.com>
2017-05-08 21:15:03 +0300

    Facebook button fix

---
+++ public/js/helpers/facebookHelper.js
@@ -0,0 +1,32 @@
+import Handlebars from 'handlebars';
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '1939533169656036',
+        xfbml      : true,
+        version    : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+Handlebars.registerHelper('facebook-btn', function(options) {
+    let html = '<script>' +
+    'function shareToFacebook() {\n' +
+    '    FB.ui( {\n' +
+    '            method: "share",\n' +
+    '            href: "' + options.fn(this) + '"\n' +
+    '        },\n' +
+    '        function(response){});\n' +
+    '}\n' +
+    '</script>\n' +
+    '<button class="btn btn-primary item-btn-share" onclick="shareToFacebook()">Facebook</button>';
+    return new Handlebars.SafeString( html );
+});
